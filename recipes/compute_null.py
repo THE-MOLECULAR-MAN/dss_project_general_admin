52df25a90043591a42cfb2e54a45860cfe858059
demo <tim.honker@dataiku.com>
2025-09-16 17:14:27 +0000

    Recipe HONKER_GENERAL_DSS_ADMIN.compute_null saved through notebook rebuild_all_code_environments

--- recipes/compute_null.py
+++ recipes/compute_null.py
@@ -1,69 +1,81 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 import os
-import json
 import dataiku
-import os
 from concurrent.futures import ThreadPoolExecutor
 
-def get_directory_size(path):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(path, followlinks=False):
-        for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
-            try:
-                if not os.path.islink(file_path):  # skip symlinks
-                    total_size += os.path.getsize(file_path)
-            except FileNotFoundError:
-                continue
-    return total_size
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Configuration
+force_rebuild_env = False
 
-def human_readable_size(size_bytes):
-    for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
-        if size_bytes < 1024:
-            return f"{size_bytes:.2f} {unit}"
-        size_bytes /= 1024
-    return f"{size_bytes:.2f} EB"
-
-def _process_code_env(code_env_info):
-    try:
-        # env_number += 1
-        envName = code_env_info['envName']
-        code_env = client.get_code_env(code_env_info['envLang'], envName)
-
-        # rebuild it from scratch
-        print(f'Starting rebuilding {envName} ...')
-        env_path = os.path.join('/data/dataiku/dss_data/code-envs/python', envName)     
-       
-        size_before_bytes = get_directory_size(env_path)
-        res = code_env.update_packages(force_rebuild_env=True)
-
-        if not res['messages']['success']:
-            failed_builds.append(envName)
-            print(res)
-        else:
-            size_after_bytes = get_directory_size(env_path)
-            # print(f"{envName} size before: {human_readable_size(size_before_bytes)}  Size after: {human_readable_size(size_after_bytes)}\n")
-            sum_size_before_bytes += size_before_bytes
-            sum_size_after_bytes  += size_after_bytes
-       
-    except Exception as e:
-        print(f"Exception: {e}")
-        # continue
-        pass
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 client = dataiku.api_client()
 code_envs = client.list_code_envs()
 
-sum_size_before_bytes = 0
-sum_size_after_bytes = 0
-# env_number = 0
-# total_num_env = len(code_envs)
-failed_builds = []
+failed_builds = set()
+successful_builds = set()
 
-max_workers = os.cpu_count() or 1
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+def _process_code_env(code_env_info):
+    try:
+        client = dataiku.api_client()
+        envName = code_env_info['envName']
+        code_env = client.get_code_env(code_env_info['envLang'], envName)
+
+        # rebuild it
+        # print(f'Starting rebuilding {envName} ...')
+        # env_path = os.path.join('/data/dataiku/dss_data/code-envs/python', envName)
+
+        res = code_env.update_packages(force_rebuild_env=force_rebuild_env)
+
+        if res['messages']['success']:
+            print(f'Success: {envName}')
+            successful_builds.add(envName)
+        else:
+            print(f"FAILED: {envName}")
+            failed_builds.add(envName)
+
+    except Exception as e:
+        try:
+            if not force_rebuild_env:
+                # print(f'Failed to build {envName} without force rebuild, trying again with force rebuild...')
+                res = code_env.update_packages(force_rebuild_env=True)
+                print(f'SUCCESS on second attempt when force rebuilding {envName}')
+                pass
+        except Exception as e:
+            print(f"Still failed to build {envName}, even with force rebuild:\n   {e}")
+            failed_builds.add(envName) # potential bug where this doesn't happen, should use a finally clause
+            pass
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# >2 multi-threading causes /tmp to balloon in disk space from pip, causes / root file system to have 0% free space
+# Fleet Manager instances only have a root file system size of 31 GB, 21 of which are used by base OS
+# /tmp is mounted to /
+max_workers = 1 # os.cpu_count() or 1
 with ThreadPoolExecutor(max_workers=max_workers) as executor:
     executor.map(_process_code_env, code_envs)
-if len(failed_builds) > 0:
-    print(f'Environments that failed to build: {failed_builds}')
+    
+print('\nFinished.')
 
-print('Finished rebuilding all code environments from scratch\nTotal size before: {human_readable_size(sum_size_before_bytes)\n Total size after: {human_readable_size(sum_size_after_bytes)}\nChange: {(sum_size_after_bytes-sum_size_before_bytes)/sum_size_before_bytes}%')
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# This should probably be in the Finally clause above?
+import os
+import shutil
+import glob
+
+for d in glob.glob('/tmp/pip-*'):
+    print(f'Deleting {d}...')
+    if os.path.isdir(d):
+        shutil.rmtree(d, ignore_errors=False)
+    else:
+        os.remove(d)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+if len(failed_builds) > 0:
+    failed_str = "\n".join(sorted(failed_builds, key=str.lower))
+    
+    print(f"\n\n\nEnvironments that failed to build:\n\n{failed_str}")
+
+print('\n\n\nFinished rebuilding code environments from scratch')
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+successful_builds
