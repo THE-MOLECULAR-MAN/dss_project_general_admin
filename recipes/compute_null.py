e3dbdde05bb1e261487b7cce8d3b99b9d7693d30
demo <tim.honker@dataiku.com>
2025-09-17 14:31:29 +0000

    Recipe HONKER_GENERAL_DSS_ADMIN.compute_null saved through notebook rebuild_all_code_environments

--- recipes/compute_null.py
+++ recipes/compute_null.py
@@ -1,10 +1,24 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# # Rebuild all code environments
+# 
+# This Python Notebook creates a list of all Code Environments, then attempts to update each of them. If one fails to update, then it will retry with the "Force Rebuild" enabled which will clear the entire code environment before rebuilding it.
+# 
+# This has been tested and validated on Dataiku DSS v14.1.2, running on Alma and Ubuntu Linux. It should work with the "Python 3 (ipykernel)" built-in code environment.
+# 
+# Written by Tim H - Sept 2025
+
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import shutil
+import glob
 import os
 import dataiku
 from concurrent.futures import ThreadPoolExecutor
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# # Configuration
+
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Configuration
+# Leave this as false and it will automatically attempt to switch it to True if the build fails
 force_rebuild_env = False
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
@@ -16,13 +30,10 @@ successful_builds = set()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 def clear_pip_tmp():
-    # This should probably be in the Finally clause above?
-    import os
-    import shutil
-    import glob
-
+    # This function deletes all the temporary files created by Pip during the installation process. They are not always cleared and when dealing with dozens of Code Environments, it can fill up the hard disk very quickly.
+    
     for d in glob.glob('/tmp/pip-*'):
-        print(f'Deleting {d}...')
+        # print(f'Deleting {d}...')
         if os.path.isdir(d):
             shutil.rmtree(d, ignore_errors=True)
         else:
@@ -34,10 +45,10 @@ def _process_code_env(code_env_info):
         envName = code_env_info['envName']
         code_env = client.get_code_env(code_env_info['envLang'], envName)
 
-        # rebuild it
         # print(f'Starting rebuilding {envName} ...')
         # env_path = os.path.join('/data/dataiku/dss_data/code-envs/python', envName)
 
+        # rebuild it
         res = code_env.update_packages(force_rebuild_env=force_rebuild_env)
 
         if res['messages']['success']:
@@ -52,10 +63,10 @@ def _process_code_env(code_env_info):
             if not force_rebuild_env:
                 # print(f'Failed to build {envName} without force rebuild, trying again with force rebuild...')
                 res = code_env.update_packages(force_rebuild_env=True)
-                print(f'SUCCESS on second attempt when force rebuilding {envName}')
+                print(f'Success: {envName} when Force rebuild')
                 pass
         except Exception as e:
-            print(f"Still failed to build {envName}, even with force rebuild:\n   {e}")
+            print(f"FAILED: {envName}, even with force rebuild")
             failed_builds.add(envName) # potential bug where this doesn't happen, should use a finally clause
             pass
 
@@ -72,12 +83,7 @@ print('\nFinished.')
 clear_pip_tmp() # clean up
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-if len(failed_builds) > 0:
-    failed_str = "\n".join(sorted(failed_builds, key=str.lower))
-    
-    print(f"\n\n\nEnvironments that failed to build:\n\n{failed_str}")
-
-print('\n\n\nFinished rebuilding code environments from scratch')
+failed_builds
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 successful_builds
